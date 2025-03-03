76da9e3f986d516a380d73de8a3ea294aad488ed
dandobai <dandobai@gmail.com>
2025-03-03 03:11:48 +0100

    feat: implement authentication and custom user details service

---
+++ user-handler/src/main/java/com/dandobai/user_handler/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.dandobai.user_handler.services;
+
+import com.dandobai.user_handler.models.MyUserDetails;
+import com.dandobai.user_handler.models.User;
+import com.dandobai.user_handler.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+
+        return new MyUserDetails(user.get());
+    }
+}
